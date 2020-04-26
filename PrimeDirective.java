051c32e3619346c3807ea81e3f22378bdc6e4900
Tate Capra <tatecapra@outlook.com>
2020-04-25 23:07:56 -0500

    added finished primedir pratice

--- PrimeDirective.java
+++ PrimeDirective.java
@@ -1,23 +1,42 @@
 // Import statement:
 import java.util.ArrayList;
 
-public class PrimeDirective{
+class PrimeDirective {
 
   // Add your methods here:
+
+  // method returns boolean
   public boolean isPrime(int number){
-
-
-    //for statement to run through array
-    for(int i = 0; i < number; i++){
-      // if statement to check prime number
-      // number is check against i and returns false
-      if(number % i == 0 && number == 2){
+    //if statement check to see if the number is 2
+    if(number == 2){
+      return true;
+    }else if(number < 2){
+      return false;
+    }
+    // checks to see if can only be div by self
+      //checks numbers from 2 to one number belowself
+    for(int i = 2; i < number; i++){
+      if(number % i == 0){
         return false;
-      }else if(number < 2){
-        return true;
       }
     }
-     return true;
+    return true;
+  }
+  // creats an empty array list named onlyPrimes
+  // takes in the int of numbers
+  public ArrayList<Integer> onlyPrimes(int[] numbers){
+    // new array list to hold primes
+    ArrayList<Integer> primes = new ArrayList<Integer>();
+    // for each loop to add the prime to array
+    for(int number : numbers){
+      // use isPrime to check the number
+      if(isPrime(number) == true){
+        //adds number to numbers array
+        primes.add(number);
+
+      }
+    }
+    return primes;
   }
 
   public static void main(String[] args) {
@@ -25,7 +44,13 @@ public class PrimeDirective{
     PrimeDirective pd = new PrimeDirective();
     int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
 
+    //TEST
     System.out.println(pd.isPrime(6));
+
+    //Test for Arraylist
+    // calls the object pb and then uses only primes to check the numbers in the array
+    System.out.print(pd.onlyPrimes(numbers));
+
   }
 
 }
